4a3e105ff4d7f8930f5f6f01c1fc3d330f7f3086
Vishal Sharma <vishalsharma@Vishals-MacBook-Pro.local>
2019-01-06 19:35:41 +0530

    React with Webpack and Babel Boilerplate

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'VISHAL SHARMA:- REACT WITH WEBPACK AND BABEL';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
